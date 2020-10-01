a1a3d252280158f4eaafd2c4d7a7aa6f29d5bc54
Rajat Sachdeva <rajatsachdeva3105@gmail.com>
2020-10-01 10:48:02 +0530

    Create mergeSort.py

---
+++ Sorting/mergeSort.py
@@ -0,0 +1,29 @@
+def merge(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+        merge(left)
+        merge(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+print('Enter the numbers seperated by space: ',end = '')
+arr = [int(x) for x in input().split()]
+merge(arr)
+print('Sorted array:',*arr)
